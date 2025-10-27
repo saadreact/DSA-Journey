f5f8d5e82ba435baa840ef57ddb9cdef512c7865
Syed Saad Shah <saadshah201@gmail.com>
2025-10-27 23:33:27 +0500

    In, Pre , Post Order BFS

--- DataStructures/Trees/binarySearchTree.js
+++ DataStructures/Trees/binarySearchTree.js
@@ -76,23 +76,78 @@ export class BinarySearchTree {
     return list;
   }
 
-  breadthFirstSearchR(queue, list){
+  breadthFirstSearchR(queue, list) {
     //  create base case to stop
-    if(!queue.length) return list;
+    if (!queue.length) return list;
 
     let currentNode = queue.shift();
     list.push(currentNode.value);
-    
-    if(currentNode.left) {
-      queue.push(currentNode.left)
-    
+
+    if (currentNode.left) {
+      queue.push(currentNode.left);
     }
-    if(currentNode.right) {
-      queue.push(currentNode.right)
+    if (currentNode.right) {
+      queue.push(currentNode.right);
     }
-    return this.breadthFirstSearch(queue,list)
+    return this.breadthFirstSearch(queue, list);
   }
 
+  DFSInorder() {
+    return traverseInOrder(this.root, []);
+  }
+
+  DFSPreorder() {
+    return traversePreOrder(this.root, []);
+  }
+
+  DFSPostorder() {
+    return traversePostOrder(this.root, []);
+  }
+}
+
+//     9
+//  4     20
+//1  6  15  170
+
+function traverseInOrder(node, list) {
+  if (node.left) {
+    traverseInOrder(node.left, list);
+  }
+
+  list.push(node.value);
+
+  if (node.right) {
+    traverseInOrder(node.right, list);
+  }
+
+  return list;
+}
+
+function traversePreOrder(node, list) {
+  list.push(node.value);
+
+  if (node.left) {
+    traversePreOrder(node.left, list);
+  }
+  if (node.right) {
+    traversePreOrder(node.right, list);
+  }
+
+  return list;
+}
+
+function traversePostOrder(node, list) {
+  if (node.left) {
+    traversePostOrder(node.left, list);
+  }
+
+  if (node.right) {
+    traversePostOrder(node.right, list);
+  }
+
+  list.push(node.value);
+
+  return list;
 }
 
 // const tree = new BinarySearchTree();
@@ -110,7 +165,13 @@ export class BinarySearchTree {
 // //  4     20
 // //1  6  15  170
 // [9, 4, 1, 6, 20, 15, 170]
+
 // BFS [9, 4, 20, 1, 6, 15, 170]
+
+// DFS - Inorder - [1, 4, 6, 9 , 15, 20 ,170]
+// DFS - Pre order - [9, 4, 1, 6, 20, 170, 15]
+// DFS - Post order - [1, 6, 4, 15, 170, 20, 9]
+
 // console.log(tree.breadthFirstSearch());
 
 export function traverse(node) {
